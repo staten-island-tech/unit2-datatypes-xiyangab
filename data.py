0b191f6a4d84dcaea7e288b5caa7b5ece71e7bd7
xiyangab <xiyangh@nycstudents.net>
2025-09-16 06:25:58 -0700

    still doing challenge
    almost finished

---
+++ data.py
@@ -0,0 +1,26 @@
+#integers and floats
+""" x = 3
+y = float(3) """
+""" print(x,y) """
+
+#lists
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+ """
+#what if we just want one single, specific value?
+""" print(values[0])
+print(values[6]) """
+
+#string methods
+""" x = "this is a thing"
+y = x.split( )
+z = y[0]
+print(y)
+print(z)
+ """
+#challenge
+sentence = input("Please input any sentence.")
+sentence.split( )
+print(len(sentence))
